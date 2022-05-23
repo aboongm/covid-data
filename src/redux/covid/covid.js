12245facfc3d3130cef405742f467bc94d5083c2
aboongm <aboongm@yahoo.com>
2022-05-23 18:07:07 +0530

    Install axios, framer-motion, and prop-types, update covid.js, Home.js, Countries.js, and Details.js

--- src/redux/covid/covid.js
+++ src/redux/covid/covid.js
@@ -1,8 +1,40 @@
-const initialState = [];
+import Axios from 'axios';
+
+const baseURL = 'https://api.covid19tracking.narrativa.com/api/2022-05-22';
+
+// constants
+const FETCH_DATA = 'covid/covid/FETCH_DATA';
+
+// initial-state
+const initialState = {
+  datesData: {},
+  totalData: {},
+};
+
+// actions
+const fetchDataAction = (dates, total) => ({
+  type: FETCH_DATA,
+  dates,
+  total,
+});
+
+export const fetchDataApi = () => async (dispatch) => {
+  const returnValue = await Axios.get(baseURL);
+  const {
+    data: { dates, total },
+  } = returnValue;
+  dispatch(fetchDataAction(dates, total));
+};
 
 // reducer
 const covidReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_DATA:
+      return {
+        ...state,
+        datesData: action.dates['2022-05-22'].countries,
+        totalData: action.total,
+      };
     default:
       return state;
   }
